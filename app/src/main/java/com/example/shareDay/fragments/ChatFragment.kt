59c4d9337cfea98ede9d626ee8f2df9fd1e249c6
Administrator <julia747757@gmail.com>
2021-08-13 02:56:45 +0900

    (8.13) chat fragment에 recyclerView가 안 뜬다

--- app/src/main/java/com/example/shareDay/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/shareDay/fragments/ChatFragment.kt
@@ -1,10 +1,43 @@
 package com.example.shareDay.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shareDay.R
+import com.example.shareDay.chats.UserInfo
+import com.example.shareDay.chats.UserListAdapter
+import java.util.ArrayList
 
 class ChatFragment : Fragment() {
 
+    var userList: RecyclerView? = null
+    var adapter: UserListAdapter? = null
+    var items: ArrayList<UserInfo> = ArrayList<UserInfo>()
+
+    private var linearLayoutManager: RecyclerView.LayoutManager? = null
+    private var recyclerAdapter: RecyclerView.Adapter<UserListAdapter.Holder>? = null
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.chat_fragment, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.list)
+
+        recyclerAdapter = UserListAdapter(userList)
+        linearLayoutManager = LinearLayoutManager(activity)
+
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = recyclerAdapter
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
