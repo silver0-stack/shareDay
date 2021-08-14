583ec922b622f95fff6ef3dca9c204b4db540095
Administrator <julia747757@gmail.com>
2021-08-15 04:23:17 +0900

    채팅목록 데이터베이스 하는 중

--- app/src/main/java/com/example/shareDay/chats/UserInfo.kt
+++ app/src/main/java/com/example/shareDay/chats/UserInfo.kt
@@ -3,16 +3,9 @@ package com.example.shareDay.chats
 import android.graphics.drawable.Drawable
 //test
 
-data class UserInfo(val name: String?) {
+data class UserInfo(var name: String?=null, var profile: String?=null, var about: String?=null) {
 
     /*var name: String? = null
     var profile: Drawable? = null
-    var about: String? = null
-
-    constructor() {}
-    constructor(name: String?, profile: Drawable, about: String?) {
-        this.name = name
-        this.profile = profile
-        this.about = about
-    }*/
+    var about: String? = null*/
 }
--- app/src/main/java/com/example/shareDay/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/shareDay/fragments/ChatFragment.kt
@@ -10,18 +10,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.shareDay.R
 import com.example.shareDay.UserListAdapter
 import com.example.shareDay.chats.UserInfo
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.ArrayList
 
 class ChatFragment : Fragment(R.layout.chat_fragment), UserListAdapter.ClickListener {
-//ㅅㄷㄴㅅ
+
     private lateinit var adapter: UserListAdapter
     val listData: ArrayList<UserInfo> = ArrayList()
+
+    private lateinit var db: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let{
 
         }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -42,12 +48,27 @@ class ChatFragment : Fragment(R.layout.chat_fragment), UserListAdapter.ClickList
     }
 
     private fun buildDisplayData(){
-        listData.add(UserInfo("test1"))
-        listData.add(UserInfo("test2"))
-        listData.add(UserInfo("test3"))
+
+        db = FirebaseDatabase.getInstance().reference
+        db.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
+
+        listData.add(UserInfo("test1", "","ttttttt"))
+        listData.add(UserInfo("test2", "", "eeeeeeeeeee"))
+        listData.add(UserInfo("test3", "","sssssssssst" ))
     }
 
-    //커밋테스트~
     override fun onItemClick(dataModel: UserInfo) {
         //val fragment: Fragment = DetailFragment.newInstance(UserInfo.name!!)
         val transaction = activity?.supportFragmentManager!!.beginTransaction()
