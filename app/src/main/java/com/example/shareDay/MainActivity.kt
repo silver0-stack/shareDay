fdc39c18740dacdf2e99bf7ba7e45fde68d03710
Administrator <julia747757@gmail.com>
2021-08-15 05:42:32 +0900

    채팅목록 데이터베이스 하는 중

--- app/src/main/java/com/example/shareDay/MainActivity.kt
+++ app/src/main/java/com/example/shareDay/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.shareDay
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/shareDay/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/shareDay/fragments/ChatFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,8 @@ class ChatFragment : Fragment(R.layout.chat_fragment), UserListAdapter.ClickList
     private lateinit var adapter: UserListAdapter
     val listData: ArrayList<UserInfo> = ArrayList()
 
+    private val sendButton: Button? = null
+
     private lateinit var db: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +30,7 @@ class ChatFragment : Fragment(R.layout.chat_fragment), UserListAdapter.ClickList
         arguments?.let{
 
         }
+
 
     }
 
